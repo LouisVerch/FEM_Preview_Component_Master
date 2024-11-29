6c836c0b48bf192a60773dfa01568f31f08f2be0
LouisVerch <louisverchere@hotmail.fr>
2024-11-29 15:48:11 +0100

    Final commit for now might need some refacto later espcially on JS. HAppy with the result currently

--- main.js
+++ main.js
@@ -4,20 +4,24 @@ const box = document.getElementById("box");
 const pop_up = document.getElementById("pop-up");
 const footer = document.getElementById("footer-container");
 const close_btn = document.getElementById("btn-close");
+// Constant for the second class of close_button for foot-container
+const close_btn_2 = document.getElementById("btn-close-2");
 // variable that measures if screen size > tablet
-let isLargeScreen = window.innerWidth > 768;
+let isLargeScreen = window.innerWidth >= 768;
 
 // updates isLargeScreen when window is resized
 window.addEventListener("resize", () => {
   isLargeScreen = window.innerWidth >= 768;
-  if (isLargeScreen === true) {
+  if (window.innerWidth >= 768) {
     box.style.display = "none";
     footer.style.display = "flex";
-
-    // Debugging of the fonction
-    //   console.log("if activated");
-    //   console.log(box.style.display);
+    close_btn.click();
+    close_btn_2.click();
   }
+  // Debugging of the fonction
+  // console.log("if activated");
+  // console.log(window.innerWidth);
+  // console.log(isLargeScreen);
   if (isLargeScreen === false) {
     pop_up.style.display = "none";
   }
@@ -32,32 +36,37 @@ toggle_btn.addEventListener("click", () => {
     (box.style.display === "none" || box.style.display === "")
   ) {
     console.log("if 1");
-    box.style.display = "flex"; // Show the box
     footer.style.display = "none"; //Hide the footer
+    box.style.display = "flex"; // Show the box
     close_btn.style.display = "flex";
+    close_btn_2.style.display = "flex";
   } else {
     if (
       (pop_up.style.display === "none" || pop_up.style.display === "") &&
       isLargeScreen === true
     ) {
-      console.log("if 3");
+      console.log("if 2");
       pop_up.style.display = "flex";
       toggle_btn.style.display = "none";
       close_btn.style.display = "flex";
-    } else {
-      console.log("if 4)");
-      pop_up.style.display = "none";
-      toggle_btn.style.display = "flex";
+      close_btn_2.style.display = "flex";
     }
   }
 });
 close_btn.addEventListener("click", () => {
+  console.log("if3");
+  pop_up.style.display = "none";
+  toggle_btn.style.display = "flex";
+  close_btn.style.display = "none";
+});
+close_btn_2.addEventListener("click", () => {
   if (box.style.display === "flex" && isLargeScreen === false) {
-    console.log("if5");
+    console.log("if4");
     box.style.display = "none"; // Show the box
     footer.style.display = "flex"; //Hide the footer
+    close_btn.style.display = "none";
   } else {
-    console.log("if6");
+    console.log("if5");
     pop_up.style.display = "none";
     toggle_btn.style.display = "flex";
     close_btn.style.display = "none";
